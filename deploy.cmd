0fb7c878cb7ef08e80346f9dc07c01ea47124a34
Will <doggy.huang@gmail.com>
2022-06-24 21:21:39 +0800

    update 2

--- deploy.cmd
+++ deploy.cmd
@@ -90,8 +90,10 @@ echo Handling node.js deployment.
 
 :: 1. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  pushd "%DEPLOYMENT_SOURCE%"
   call :ExecuteCmd !NPM_CMD! install --production
   call :ExecuteCmd !NPM_CMD! run build -- --output-path "%DEPLOYMENT_SOURCE%\dist"
+  popd
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
